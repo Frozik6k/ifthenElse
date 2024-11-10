2c00a8a0075e4fef0e09fa9dfa7de9c1c24f3883
Frozik6k <mgc2000@bk.ru>
2024-11-10 21:05:04 +0500

    Исправил ошибки в 1 и 2 задаче

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,33 @@ public class Main {
         System.out.println("Задача №1");
         int clientOS = 0;
         if (clientOS == 0){
-            System.out.println("Установите версию приложения для IOS по ссылке");
-        } else if (clientOS == 1){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1){
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else{
-            System.out.println("У вас неизвестная операционная система, зайдите либо под Android, либо под IOS");
+        }
+        if (clientOS != 0 && clientOS != 1){
+            System.out.println("Не правильный формат");
         }
 
         // task 2
         System.out.println("Задача №2");
         int clientDeviceYear = 2015;
-        if (clientDeviceYear >= 2015){
-            if (clientOS == 0){
-                System.out.println("Установите версию приложения для IOS по ссылке");
-            } else {
-                System.out.println("Установите версию приложения для Android по ссылке");
-            }
-        } else {
-            if (clientOS == 0){
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-            } else {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");
-            }
+        clientOS = 0;
+
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
+        if (clientOS == 0 && clientDeviceYear >= 2015){
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear < 2015){
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+        if (clientOS == 1 && clientDeviceYear >= 2015){
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
 
         // task 3
         System.out.println("Задача №3");
